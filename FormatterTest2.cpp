2f0e4d537d1ac7f5b5f8a326d67db97bc31ac7d6
Yubing(Jackie) Xie <yubingxi@usc.edu>
2023-12-01 17:33:55 -0800

    asd

--- FormatterTest2.cpp
+++ FormatterTest2.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
-#if WITH_EDITOR
-int i = 9;
-int j = 999999;
-#endif
-void MyTestFunction()
-{
-	if (false)
-	{
-		td::cout << "hellow";
-	}
+  #if WITH_EDITOR
+int i =   9;  
+int j    = 129 ;    
+  #endif   
+void MyTestFunction() {
+    if (false) { 
+  td::cout << "hellow";
+    } 
 }
