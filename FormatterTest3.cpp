bdb52f270fefb712d4298d9f727cc453279fa18a
Yubing(Jackie) Xie <yubingxi@usc.edu>
2023-12-05 14:25:22 -0800

    Update FormatterTest3.cpp

--- FormatterTest3.cpp
+++ FormatterTest3.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
-#if WITH_EDITOR
-int i = 9;
-int j = 8111;
-#endif
-void MyTestFunction()
-{
+	#if WITH_EDITOR
+	int i		 = 9  ;
+int j = 8119981;		
+#endif    
+void MyTestFunction(){	
 	if (false)
 	{
-		td::cout << "hellow";
+		std::cout << "hellow";
 	}
 }
